3d5668d78859614a9f0e1bb8999ecc1fa8601eba
lullaby <naomi-myuzaki@hotmail.fr>
2016-05-07 22:00:27 +0000

    Debut de commentaires, a finir

--- integration.py
+++ integration.py
@@ -1,44 +1,57 @@
 from math import *
 import splint as sp
 
-def deriv(f, step):
-    """ Derives a function f 
-    f : a function, represented by a tabular where step is the space between two adjacent abscissas 
+def deriv(f, step=0.001):
+    """ The "deriv" function takes 2 arguments :
+        - f: array of real, step is the space between two adjacent abscissas
+        - step: real, step for x
+        Returns fprim, the derivate of the function f ... <- TO CHECK
     """
-    
     fprim=[]
-    fprim.append( (f[1] - f[0]) / step ) # First special case : derivative of the function on the first point.
+    fprim.append( (f[1] - f[0]) / step ) # First special case: derivative of the function on the first point.
 
     for i in range (1, len(f)-1):
-        fprim.append( (f[i+1] - f[i-1]) / step ) # General case : approximation of the derivative on a point with the adjacent points.
+        fprim.append( (f[i+1] - f[i-1]) / step ) # General case: approximation of the derivative on a point with the adjacent points.
 
     fprim.append( (f[len(f)-1] - f[len(f)-2]) / step ) # Second special case : derivative of the function on the last point.
     
     return fprim
 
 
-def length(spline, method, step):
+def length(spline, method, step=0.001):
+    """ The "length" function takes 3 arguments :
+        - spline: array of real, contains the ordinate of the points of the given part
+        - method: function to use ... <- TO CHECK
+        - step: real, step for x
+        Returns ... <- TO CHECK
+    """
     fprim = deriv(spline, step)
-
     g = map(lambda x: sqrt( 1 + x**2 ), fprim)
-
     return method(g, step)
 
 
-def trapezium(f, step):
+
+def trapezium(f, step=0.001):
+    """ The "trapezium" function takes 2 arguments :
+        - f: ... <- TO CHECK
+        - step: real, step for x
+        Returns I, ... <- TO CHECK
+    """
 # midpoint rule approximation method of an integral
-
     I = 0 
-
     for i in range(len(f)-1): 
         I += step * (f[i] + f[i+1]) / 2
-
     return I
+
+
 
 def test():
     step = 10E-2
     (spline1, spline2) = sp.wings_interpolation("DU84132V.DAT", step)
-    print "upper :" , length(spline1, trapezium, step)
-    print "lower :" , length(spline2, trapezium, step)
+    print "Upper:" , length(spline1, trapezium, step)
+    print "Lower:" , length(spline2, trapezium, step)
 
-#test()
+
+
+if __name__ ==  '__main__':
+    test()
